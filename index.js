f51156a4cc3e32d76280ed545beb46f4d2cbc98e
Rishabh Sharma <rishabhsharma3617@gmail.com>
2020-04-07 15:16:38 +0530

    Done

--- index.js
+++ index.js
@@ -8,18 +8,41 @@ const REDIS_PORT = process.env.PORT || 6379;
 const client = redis.createClient(REDIS_PORT)
 const app = express()
 
-app.get('/repos/:username' , (req,res,next) => {
+
+//cache middleware
+function cache (req,res,next) {
+    const {username } = req.params
+    client.get(username , (err ,data) => {
+        if(err)
+        {
+            throw err;
+        }
+        if(data !== null)
+        {
+            res.send(`<h1>${username} has ${data} repos</h1>`)
+        }
+        else
+        {
+            next()
+        }
+    })    
+}
+
+
+app.get('/repos/:username' ,cache ,  async (req,res,next) => {
     try {
-        const { username }  = req.params
-
-        fetch(`https://api.github.com/users/${username}`).then((response) => {
-            console.log(response)
-            res.send(response.json())
-        }).catch(err => console.log(err)) 
-
         console.log("Fetching the data from the api")
+        const { username }  = req.params
+        const response = await fetch(`https://api.github.com/users/${username}`)
+        const data = await response.json()
+        const repos = data.public_repos
+
+        //Set the data to the redis
+        client.setex(username , 3600 , repos)
+
+        res.send(`<h1>${username} has ${repos} repos</h1>`)
     } catch (error) {
-        
+        console.log(error)
     }
 })
 
